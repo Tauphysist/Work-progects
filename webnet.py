63ed09e955ce47db57f16a0aa68d72daae23ff7c
Tayphysist <43246449+Tauphysist@users.noreply.github.com>
2022-12-02 15:35:30 -0500

    Fix of the stupid mistake

--- webnet.py
+++ webnet.py
@@ -6,12 +6,13 @@ r = requests.get("websocet")
 # print((r.json()))
 print((r.json()['token']))
 token=(r.json()['token'])
-message={ "quantBotUserId":"id", "token":token, "type":"Login"}
+userid=r.json()["quantBotUserId"]
+message={ "quantBotUserId":userid, "token":token, "type":"Login"}
 send=json.dumps(message)
 # print(send)
 # print(type(send))
 
-def worker(cur,tradetraker,price_control):
+def worker(cur,tradetrakerq,price_control):
     global reader,j
     httpsell='http://localhost:8001/order/place?Side=Sell&Qty=20000&Symbol='+cur
     httpbuy = 'http://localhost:8001/order/place?Side=Buy&Qty=20000&Symbol=' + cur
@@ -19,20 +20,20 @@ def worker(cur,tradetraker,price_control):
     if reader["type"] == "GaugeUpdate" and "Symbol" in reader:# and rev.json()['OpenQuantity']==0:
 
         # print(tradetraker )
-        if tradetraker == 0 and reader["Symbol"] == cur:
+        if tradetrakerq == 0 and reader["Symbol"] == cur:
             # print("HEEEEEEEEEy")
-            # print(reader["price"])
+            print(reader["Symbol"])
             if reader["EntryProgress"] > 0.76:
                 print(reader)
                 r = requests.get(httpsell)
-                tradetraker = 1
+                tradetrakerq = 1
 
                 price_control=reader["Price"]
 
 
             elif reader["EntryProgress"] <- 0.76:
                 r = requests.get(httpbuy)
-                tradetraker = 2
+                tradetrakerq = 2
                 print(reader)
                 price_control=reader["Price"]
 
@@ -45,55 +46,59 @@ def worker(cur,tradetraker,price_control):
             pricer = reader["Price"]
         if reader["Symbol"] == cur:
 
-            if tradetraker == 1:
+            if tradetrakerq == 1:
                 # print("got it")
                 price_diff = price_control - pricer
-                print(1)
-                print(price_diff)
+                # print(1)
+                # print(price_diff)
                 if price_diff >= 0.0005:
                     r = requests.get(httpbuy)
-                    tradetraker = 0
+                    tradetrakerq = 0
                     price_control=0
+                    print(price_diff)
                 elif price_diff <= -0.0005:
                     r = requests.get(httpbuy)
-                    tradetraker = 0
+                    tradetrakerq = 0
                     price_control = 0
+                    print(price_diff)
                 # print((r.json()))
                 # print((r.json()['AvailableFunds']))
-            elif tradetraker == 2:
+            elif tradetrakerq == 2:
 
                 # print(reader)
                 # print((r.json()))
                 # print((r.json()['AvailableFunds']))
                 price_diff = pricer - price_control
-                print(2)
-                print(price_diff)
+                # print(2)
+                # print(price_diff)
                 if price_diff >= 0.0005:
                     r = requests.get(httpsell)
-                    tradetraker = 0
+                    tradetrakerq = 0
                     price_control = 0
+                    print(price_diff)
                 elif price_diff <= -0.0005:
                     r = requests.get(httpsell)
-                    tradetraker = 0
+                    tradetrakerq = 0
                     price_control = 0
+                    print(price_diff)
 
                 # print((r.json()))
                 # print((r.json()['AvailableFunds']))
 
-    if reader["type"] == "OrderExecution" and tradetraker != 0 and reader["Symbol"] == cur and reader["exit"] == "True":
+    if reader["type"] == "OrderExecution" and tradetrakerq != 0 and reader["Symbol"] == cur and reader["exit"] == "True":
         print("HEEEEEEEEEy")
         if reader["executionType"] == "SELL":
             r = requests.get(httpbuy)
-            tradetraker = 0
+            tradetrakerq = 0
             price_control = 0
         else:
             r = requests.get(httpsell)
             tradetraker = 0
             price_control = 0
     # print(price_control)
-    return(tradetraker,price_control)
+    return(tradetrakerq,price_control)
 
-ws = create_connection("wss://qbws.stealthtrader.com/")
+ws = create_connection("Web socket")
 ws.send(send)
 result = ws.recv()
 print(result)
@@ -126,7 +131,7 @@ while True:
     # r = requests.get("http://localhost:8001/account/status")
     # print((r.json())[])
     # print(reader)
-    # if reader["type"] == "GaugeUpdate":
-    #     if reader["EntryProgress"]<-0.77 or reader["EntryProgress"]>0.77:
-    #         print(result)
+    if reader["type"] == "GaugeUpdate":
+        # if reader["EntryProgress"]<-0.77 or reader["EntryProgress"]>0.77:
+            print(reader["Symbol"])
 
